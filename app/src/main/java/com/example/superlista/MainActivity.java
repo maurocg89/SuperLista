c6b2005de069a77f05be832177c7f7bd16bc0a5b
FedericoBorda <federicoborda3@hotmail.com>
2017-04-11 18:39:54 -0300

    Terminando con el Navigation View y corrigiendo errores

--- app/src/main/java/com/example/superlista/MainActivity.java
+++ app/src/main/java/com/example/superlista/MainActivity.java
@@ -1,12 +1,22 @@
 package com.example.superlista;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+
 
     Button boton;
 
@@ -14,6 +24,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Fragmento fragmento = new Fragmento();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame, fragmento);
+        transaction.commit();
+
+
+
+        /*BOTON QUE CREO MAURO
         boton = (Button) findViewById(R.id.buttonPruebaDb);
         boton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -21,6 +41,6 @@ public class MainActivity extends AppCompatActivity {
                 Intent i = new Intent(MainActivity.this, PruebaBD.class);
                 startActivity(i);
             }
-        });
+        });*/
     }
 }
